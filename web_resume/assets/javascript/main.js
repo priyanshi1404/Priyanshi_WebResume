62e4afb2cfe53d733a02efb7ba9a4cbee831e5ba
Priyanshi Mittal <mittalpriyanshi1457@gmail.com>
2025-07-28 00:37:04 +0530

    Delete web_resume/assets/javascript/main.js

--- web_resume/assets/javascript/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $("#main-nav a").click(function () {
-    $("section").removeClass("show");
-    var target = $(this).attr("href");
-    $(target).addClass("show");
-  });
-});
